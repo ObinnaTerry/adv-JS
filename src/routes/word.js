3c7655f3cb81a95d979215dd1919ef5d01da7dba
obinnaterry <terrydona90@gmail.com>
2023-07-09 14:18:44 +0200

    Feat: Adds CRUD Methods

--- src/routes/word.js
+++ src/routes/word.js
@@ -3,7 +3,17 @@ const Router = express.Router();
 
 const WordModel = require('../models/word');
 
-Router.post('/', async (request, response) => {
+const isLogged = (request, response, next) => {
+    if (request.session.user) {
+        console.log('test');
+        next();
+    } else {
+        return response.status(401).json({'msg': "not logged !"})
+    }
+}
+
+Router.post('/', isLogged, async (request, response) => {
+
     const { word } = request.body;
 
     const wordModel = new WordModel({ 
@@ -24,5 +34,93 @@ Router.post('/', async (request, response) => {
         });
     }
 });
+
+Router.delete('/', isLogged, async (request, response) => {
+    const { word } = request.body;
+  
+    try {
+      const result = await WordModel.deleteOne({ name: word });
+  
+      if (result.deletedCount === 1) {
+        return response.status(200).json({
+          "msg": `Successfully deleted the word: ${word}`
+        });
+      } else {
+        return response.status(404).json({
+          "error": `Word not found: ${word}`
+        });
+      }
+    } catch (error) {
+      return response.status(500).json({
+        "error": error.message
+      });
+    }
+  });
+
+  Router.put('/:id', isLogged, async (request, response) => {
+    const { id } = request.params;
+    const { word } = request.body;
+  
+    try {
+      const result = await WordModel.findByIdAndUpdate(id, { name: word });
+  
+      if (result) {
+        return response.status(200).json({
+          "msg": `Successfully updated the word: ${word}`
+        });
+      } else {
+        return response.status(404).json({
+          "error": "Word not found"
+        });
+      }
+    } catch (error) {
+      return response.status(500).json({
+        "error": error.message
+      });
+    }
+  });
+  
+  Router.delete('/:id', isLogged, async (request, response) => {
+    const { id } = request.params;
+  
+    try {
+      const result = await WordModel.findByIdAndDelete(id);
+  
+      if (result) {
+        return response.status(200).json({
+          "msg": "Successfully deleted the word"
+        });
+      } else {
+        return response.status(404).json({
+          "error": "Word not found"
+        });
+      }
+    } catch (error) {
+      return response.status(500).json({
+        "error": error.message
+      });
+    }
+  });
+
+  Router.get('/:id', isLogged, async (request, response) => {
+    const { id } = request.params;
+  
+    try {
+      const word = await WordModel.findById(id);
+  
+      if (word) {
+        return response.status(200).json(word);
+      } else {
+        return response.status(404).json({
+          "error": "Word not found"
+        });
+      }
+    } catch (error) {
+      return response.status(500).json({
+        "error": error.message
+      });
+    }
+  });
+
 
 module.exports = Router;
